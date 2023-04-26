f4a3b5c24ac84c1e68e37964a57ea53847cde9c6
Alpha 2 <83462052+Alpha-2@users.noreply.github.com>
2023-04-26 10:24:46 +0200

    Update Server.cs

--- Handlers/Server.cs
+++ Handlers/Server.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -13,7 +13,7 @@ namespace tka.Handlers
     {
         public void Start()
         {
-            Log.Debug("TAK NIE");
+            Log.Debug("Plugin SanctionsLogger uruchomiony poprawnie");
         }
     }
 }
